c0998cc2d41aa102cf661d4295722481312c715f
Agnaldo Junior <agnaldo.junior@unoesc.edu.br>
2022-01-24 14:19:33 -0300

    Readme do projeto

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # fundamentos-android-parte-1
+
+Formação Android no Alura.com.br
+
+Primeiro App Android com duas activity, cadastro e lista de alunos. Salvando em um ArrayList
+
+Agnaldo Junior
---
+++ app/src/main/java/com/example/agenda/dao/AlunoDAO.java
@@ -0,0 +1,20 @@
+package com.example.agenda.dao;
+
+import com.example.agenda.model.Aluno;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AlunoDAO {
+
+    private final static List<Aluno> alunos = new ArrayList<>();
+
+    public void salva(Aluno alunoCriado) {
+        alunos.add(alunoCriado);
+    }
+
+
+    public List<Aluno> todos() {
+        return new ArrayList<>(alunos);
+    }
+}
